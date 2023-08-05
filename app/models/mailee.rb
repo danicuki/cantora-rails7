c430d2f2b7e12fc826fd0cda606f6e5f3e29369e
Daniel Cukier <danicuki@gmail.com>
2023-08-05 17:38:40 -0300

    deleted unused files

--- app/models/mailee.rb
+++
@@ -1,9 +0,0 @@
-module Mailee
-  class MaileeResource < ActiveResource::Base
-    self.site = "http://api.7bae89640dc49.cantora.mailee.me"
-  end
-  class Contact < MaileeResource
-  end
-  class List < MaileeResource
-  end
-end
--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveResource::Base
-	self.site = "http://www.jango.com"
-	self.headers["User-agent"] = "Mozilla/5.0"
-	
-end
